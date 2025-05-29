ca3ce3b79517b565f1f59aff7ec19afd503b8e1a
WillianArevalo-ui <e0105303473@uisrael.edu.ec>
2025-05-28 21:21:02 -0500

    Add files via upload

---
+++ calculoarea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main()
+        {
+            Console.Write("Ingrese el ancho del rectangulo:");
+            double ancho = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese el alto del rectangulo:");
+            double alto = Convert.ToDouble(Console.ReadLine());
+            double area = Calculararea(ancho, alto);
+            double perimetro = Calcularperimetro(ancho, alto); 
+            Console.WriteLine($"El area del rectangulo es: {area}");
+            Console.WriteLine($"El perimetro del rectangulo es: {perimetro}");
+
+
+        }
+        static double Calculararea(double ancho, double alto)
+        {
+            return ancho * alto;
+        }
+        static double Calcularperimetro(double ancho, double alto)
+        {
+            return 2 * (ancho + alto);
+        }
+
+}
